01c4e51df52f69278042390d95b658fb084a2cdb
Indra N. Utomo <Indra77ya@gmail.com>
2026-01-24 23:40:29 +0700

    update footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="col-auto ml-lg-auto">
             </div>
             <div class="col-12 col-lg-auto mt-3 mt-lg-0 text-center">
-                &copy; {{ date("Y") }} <a href="https://github.com/indra77ya" target="_blank">Indra N. Utomo</a>. All rights reserved.
+                &copy; {{ date("Y") }} E-Koperasi. All rights reserved.
             </div>
         </div>
     </div>
